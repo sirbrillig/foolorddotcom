d8d6fb9d505ea1ac8c1337b952a0c8b05deefd73
Payton Swick <payton@foolord.com>
2024-04-09 17:29:12 -0400

    Second commit

---
+++ index.js
@@ -0,0 +1,46 @@
+(() => {
+	/**
+	 * @return {HTMLElement[]}
+	 */
+	function getTerminalTextContainers() {
+		return Array.from(document.querySelectorAll(".terminal-text-container"));
+	}
+
+	/**
+	 * @param {HTMLElement} container
+	 * @param {number} delay
+	 * @return {void}
+	 */
+	function animateTerminalContainer(container, delay) {
+		const overlay = container.querySelector(".terminal-text-overlay");
+		if (!overlay) {
+			console.error("No overlay found");
+			return;
+		}
+		const overlaySvg = container.querySelector(".terminal-text-overlay svg");
+		if (!overlaySvg) {
+			console.error("No overlay svg found");
+			return;
+		}
+		const textArea = container.querySelector(".terminal-text");
+		if (!textArea) {
+			console.error("No text area found");
+			return;
+		}
+		overlaySvg.setAttribute("width", textArea.getBoundingClientRect().width);
+		setTimeout(() => {
+			overlay.style["animation-play-state"] = "running";
+		}, delay);
+	}
+
+	function startTerminal() {
+		const containers = getTerminalTextContainers();
+		let delay = 0;
+		containers.forEach((container) => {
+			animateTerminalContainer(container, delay);
+			delay += 200;
+		});
+	}
+
+	document.addEventListener("DOMContentLoaded", startTerminal);
+})();
