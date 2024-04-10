9e92a78ccafe8c205e91bded416c5e4b2b17b81f
Payton Swick <payton@foolord.com>
2024-04-09 23:32:29 -0400

    Simplify code needed for animation and update the text a bit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 	 * @return {HTMLElement[]}
 	 */
 	function getTerminalTextContainers() {
-		return Array.from(document.querySelectorAll(".terminal-text-container"));
+		return Array.from(document.querySelectorAll(".terminal-text"));
 	}
 
 	/**
@@ -12,25 +12,18 @@
 	 * @return {void}
 	 */
 	function animateTerminalContainer(container, delay) {
-		const textArea = container.querySelector(
-			`.terminal-text`
-		);
-		if (!textArea) {
-			return;
-		}
-
 		setTimeout(() => {
-			textArea.style["animation-play-state"] = "running";
+			container.style["animation-play-state"] = "running";
 		}, delay);
 	}
 
 	function startTerminal() {
-		const delayBetweenLinesMs = 200;
+		const delayBetweenContainersMs = 400;
 		const containers = getTerminalTextContainers();
 		let delay = 0;
 		containers.forEach((container) => {
 			animateTerminalContainer(container, delay);
-			delay += delayBetweenLinesMs;
+			delay += delayBetweenContainersMs;
 		});
 	}
 
